c218f3675536e3761c3c71d9e0c377c23408f8f1
phylis13 <phylisnjeri54@gmail.com>
2023-03-26 19:03:41 +0300

    powers_controller done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,29 @@
 class PowersController < ApplicationController
+
+    # GET /powers
+    def index 
+        powers = Power.all
+        render json: powers, status: :ok
+    end
+
+
+    # GET /powers/:id
+    def show 
+        power = Power.find(params[:id])
+        render json: power, status: :ok
+    end
+
+    # PATCH /powers/:id
+    def update 
+        power = Power.find(params[:id])
+        power.update!(power_params)
+        render json: power, status: :ok
+    end
+
+    private 
+
+    def power_params
+        params.permit(:description)
+    end
+
 end
