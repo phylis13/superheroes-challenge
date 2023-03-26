657d9177ed23b7b9f4ba4ffdc94d2b7bde86c9e5
phylis13 <phylisnjeri54@gmail.com>
2023-03-26 19:02:47 +0300

    heros_controller done

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,15 @@
-class HeroesController < ApplicationController
+class HerosController < ApplicationController
+
+    # GET /heroes
+    def index 
+        heros = Hero.all
+        render json: heros,status: :ok
+    end
+
+    # GET /heroes/:id
+    def show
+        hero = Hero.find(params[:id])
+            render json: hero,status: :ok, serializer: HeroPowerSerializer
+    end
+
 end
