d16814682b516a58c14220fc6f3854d095c43dd6
phylis13 <phylisnjeri54@gmail.com>
2023-03-26 19:18:17 +0300

    seeds.rb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Seeding superheroes..."
+
+# Hero Model
+wolverine = Hero.create(name: "Logan", super_name: "Wolverine")
+ironman = Hero.create(name: "Tony Stark", super_name: "Ironman")
+catwoman = Hero.create(name: "Patience P", super_name: "Catwoman")
+groot = Hero.create(name: "Tree", super_name: "Groot")
+invisiblewoman = Hero.create(name: "Susan Storm", super_name: "Invisible woman")
+
+# Power Model
+claws = Power.create(name: "Adamantium Claws", description: "Indestructible bones via adamantium-infused skeleton, retractable adamantium claws, superhuman senses and animal-like attributes & master hand-to-hand combatant + martial artist.")
+armor = Power.create(name: "Powered Iron Armor Suit", description: "Superhuman strength, speed, durability, agility, reflexes, senses, supersonic flight, energy repulsor and missile projection with regenerative life support.")
+agility = Power.create(name: "The Cat", description: "Master martial artist and hand-to-hand combatant, skilled gymnast and acrobat, utilizing whips, sharp retractable claws and climbing pitons.")
+psychokinesis = Power.create(name: "Psychokinesis", description: "Superhuman strength, stamina, endurance, durability, immortality, regenerative healing factor, elasticity/plasticity, and plant manipulation ability.")
+invisble = Power.create(name: "Invisibility", description: "Self-invisibility, projective invisibility, invisible force field projection:generation of protective invisible shields & invisible energy constructs, power to control and manipulate objects, telepathic immunity & flight.")
+
+# HeroPower Model
+HeroPower.create(hero_id: wolverine.id, power_id: claws.id, strength: "Strong")
+HeroPower.create(hero_id: ironman.id, power_id: armor.id, strength: "Average")
+HeroPower.create(hero_id: catwoman, power_id: agility.id, strength: "Weak")
+HeroPower.create(hero_id: groot.id, power_id: psychokinesis.id, strength: "Average")
+HeroPower.create(hero_id: invisiblewoman.id, power_id: invisble, strength: "Strong")
+
+puts "Superheroes seeded!"
