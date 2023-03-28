810611bd7116d69d836b5852e5907b19d125dd4c
phylis13 <phylisnjeri54@gmail.com>
2023-03-28 09:35:13 +0300

    fix comtrollers

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -8,8 +8,18 @@ class HerosController < ApplicationController
 
     # GET /heroes/:id
     def show
-        hero = Hero.find(params[:id])
+        hero = Hero.find_by(id: params[:id])
             render json: hero,status: :ok, serializer: HeroPowerSerializer
     end
-
+#GET /heroes/:id
+def show
+    @heros = Hero.find_by(id: params[:id])
+    if @heros
+      render json: @heros.as_json(include: {
+        powers: { only: [:id, :name, :description] }
+      }, only: [:id, :name, :super_name])
+    else
+      render json: { error: 'Hero not found' }, status: :not_found
+    end
+  end
 end
